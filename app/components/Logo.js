d196e33d15fb084308c065e3144fdd3d6920da63
Yehor Kharchenko <yehorkharchenko4@gmail.com>
2024-12-28 16:12:45 +0100

    fix

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="basis-1/4">
-      <Image src="/big-logo.svg" alt="Logo" width={150} height={1} />
+      <Image src="./big-logo.svg" alt="Logo" width={150} height={1} />
       <h2 className="font-bold mt-1 text-xs md:text-base">Events, Cafes, Dormitories On One Map</h2>
     </div>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+    basePath: isProd ? '/universeAGH' : '',
     output: 'export',
-    basePath: '/UniverseAGH',
-assetPrefix: '/UniverseAGH/',
   images: {
     unoptimized: true,
   },
